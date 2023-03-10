8f9232a9e16216ad78e225ba208e61a0720171e2
KobalJaloma <kirito10019@hotmail.com>
2023-03-10 10:42:37 -0600

    test

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({
     path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
 });
-console.log("Este es el usuario " + process.env);
+console.log("Este es el usuario " + process.env.NODE_ENV);
 // config.js
 export const config = {
     NODE_ENV: process.env.NODE_ENV || 'development',
@@ -18,9 +18,9 @@ export const config = {
     HOST: process.env.HOST || 'localhost',
 
     //db config 
-    DB_NAME: process.env.DB_NAME || '',
-    DB_USER: process.env.DB_USER || '',
-    DB_PASSWORD: process.env.DB_PASSWORD || '',
+    DB_NAME: process.env.DB_NAME || 'boletera',
+    DB_USER: process.env.DB_USER || 'procesos',
+    DB_PASSWORD: process.env.DB_PASSWORD || '2xK#iz&Gl715',
 
     //ports
     PORT: process.env.PORT || 8000,
