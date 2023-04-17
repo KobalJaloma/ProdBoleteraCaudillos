0773ffd24adde728f691709b3a6fb0ac9dc4f9b6
KobalJaloma <kirito10019@hotmail.com>
2023-04-17 15:20:38 -0600

    test para segunda conexion

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const db = new Sequelize(config.DB_NAME, config.DB_USER, config.DB_PASSWORD, {
 });
 
 
+console.log(config.DB_NAME_REPORTES_EMPRESARIALES);
 //nuevo enlaze de DB para los endpoints de la app de reportes empresariales
 export const reportes_empresariales = new Sequelize(config.DB_NAME_REPORTES_EMPRESARIALES, config.DB_USER, config.DB_PASSWORD, {
     host: config.HOST,
